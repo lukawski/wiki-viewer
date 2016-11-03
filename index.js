d62adf8feabc7f112f7da4cb2d814f28f6cf9dd7
Kacper Łukawski <ka.lukawski@gmail.com>
2016-11-03 18:41:36 +0100

    Wikipedia Viewer version 1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('#search').keypress(function (e) {
+    if (e.which === 13) {
+      $('#content').empty()
+      $.getJSON(
+        'https://en.wikipedia.org/w/api.php?action=opensearch&list=search&search=' + $(this).val() + '&utf8&format=json', 
+               function (r) {
+                 var results = r[1]
+                 var descriptions = r[2]
+                 var links = r[3]
+                 for(var i = 0; i < results.length; i++) {
+                    $('#content').append('<div class="result"><h3>' + results[i] + '</h3>' + descriptions[i] +'<br><a href=' + links[i] + ' target="_blank">More</a></div>')
+                 }
+      })
+    }
+  })
+})
