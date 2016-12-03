fea23bf0244adb178e67cb5c2c01b99a9e1ac31c
Kacper Łukawski <ka.lukawski@gmail.com>
2016-12-03 23:54:15 +0100

    Remembering typed phrase, not the best solution yet

--- index.js
+++ index.js
@@ -7,20 +7,27 @@ ga('create', 'UA-88419205-1', 'auto');
 ga('send', 'pageview');
 
 $(function () {
-  if (localStorage.getItem('currentSearch') !== null) $('#search').val(localStorage.getItem('currentSearch'))
+  function wikiCall (query) {
+    $('#content').empty()
+    $.getJSON(
+      'https://en.wikipedia.org/w/api.php?action=opensearch&list=search&search=' + query + '&utf8&format=json',
+      function (r) {
+        var results = r[1]
+        var descriptions = r[2]
+        var links = r[3]
+        for(var i = 0; i < results.length; i++) {
+          $('#content').append('<a href=' + links[i] + ' target="_blank"><div class="result"><h3>' + results[i] + '</h3><p>' + descriptions[i] +'</p></div></a>')
+        }
+      })
+  }
+
+  if (localStorage.getItem('currentSearch') !== null) {
+    $('#search').val(localStorage.getItem('currentSearch'))
+    wikiCall(localStorage.getItem('currentSearch'))
+  }
 
   $('#search').keyup(function (e) {
     localStorage.setItem('currentSearch', $(this).val())
-    $('#content').empty()
-    $.getJSON(
-      'https://en.wikipedia.org/w/api.php?action=opensearch&list=search&search=' + $(this).val() + '&utf8&format=json', 
-              function (r) {
-                var results = r[1]
-                var descriptions = r[2]
-                var links = r[3]
-                for(var i = 0; i < results.length; i++) {
-                  $('#content').append('<a href=' + links[i] + ' target="_blank"><div class="result"><h3>' + results[i] + '</h3><p>' + descriptions[i] +'</p></div></a>')
-                }
-      })
+    wikiCall($(this).val())
   })
 })
