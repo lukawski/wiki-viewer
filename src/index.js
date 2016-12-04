a6acaabcbb065af684754d8d1ecc1dd816487981
Kacper Łukawski <ka.lukawski@gmail.com>
2016-12-04 22:28:21 +0100

    Simple loader, handling 0 results found

--- src/index.js
+++ src/index.js
@@ -12,18 +12,30 @@ $(function () {
     $.getJSON(
       'https://en.wikipedia.org/w/api.php?action=opensearch&list=search&search=' + query + '&utf8&format=json',
       function (r) {
-        var results = r[1]
-        var descriptions = r[2]
-        var links = r[3]
-        for(var i = 0; i < results.length; i++) {
-          $('#content').append('<a href=' + links[i] + ' target="_blank"><div class="result"><h3>' + results[i] + '</h3><p>' + descriptions[i] +'</p></div></a>')
+        if (r[1].length > 0) {
+          var results = r[1]
+          var descriptions = r[2]
+          var links = r[3]
+          for (var i = 0; i < results.length; i++) {
+            $ ('#content').append ('<a href=' + links[i] + ' target="_blank"><div class="result"><h3>' + results[i] + '</h3><p>' + descriptions[i] + '</p></div></a>')
+          }
+        } else {
+          $('#content').append('<h3>No results found</h3>')
         }
       })
   }
 
-  if(localStorage.getItem('lastSearched') !== null) $('#lS').text(localStorage.getItem('lastSearched'))
+  $(document)
+    .ajaxStart(function () {
+      $('.loader').css('display', 'block')
+    })
+    .ajaxStop(function () {
+      $('.loader').css('display', 'none')
+    })
 
-  if (localStorage.getItem('currentSearch') !== null) {
+  if(localStorage.getItem('lastSearched')) $('#lS').text(localStorage.getItem('lastSearched'))
+
+  if (localStorage.getItem('currentSearch')) {
     $('#search').val(localStorage.getItem('currentSearch'))
     wikiCall(localStorage.getItem('currentSearch'))
   }
@@ -35,6 +47,6 @@ $(function () {
   $('#search').keyup(function (e) {
     localStorage.setItem('currentSearch', $(this).val())
     localStorage.setItem('lastSearched', $(this).val())
-    wikiCall($(this).val())
+    if ($(this).val().length > 0) wikiCall($(this).val())
   })
 })
