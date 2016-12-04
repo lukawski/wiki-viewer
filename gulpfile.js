464a6b92aa1c78c7fce945899661841316aa6b4b
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-04 19:21:54 +0100

    :zap: watch task, moved css to separate task, cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css')
+var htmlmin = require('gulp-htmlmin')
 
-gulp.task('default', ['js', 'html'], function () {
-  return gulp.src('./src/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist'))
+var paths = {
+  js: ['./src/**/*.js'],
+  html: ['./src/**/*.html'],
+  css: ['./src/**/*.css']
+}
+
+gulp.task('default', ['js', 'html', 'css'], function () {
+  return true
 })
 
 gulp.task('js', function () {
-  return gulp.src('./src/*.js')
+  return gulp.src(paths.js)
     .pipe(uglify())
     .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('html', function () {
-  return gulp.src('./src/*.html')
+  return gulp.src(paths.html)
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('css', function () {
+  return gulp.src(paths.css)
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('watch', ['js', 'html', 'css'], function () {
+  gulp.watch(paths.js, ['js'])
+  gulp.watch(paths.html, ['html'])
+  gulp.watch(paths.css, ['css'])
 })
